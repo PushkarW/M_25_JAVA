30ed32df5cef2f93452be372eb1f9243505bacae
ASUS <ASUS@LAPTOP-545B8LDK>
2022-11-07 12:26:06 +0530

    First Commit

---
+++ Day_1_ConnectingEclipseWithGitHub/src/org/tnsindia/demo/Demo.java
@@ -0,0 +1,10 @@
+package org.tnsindia.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome To Eclipse");
+	}
+
+}
